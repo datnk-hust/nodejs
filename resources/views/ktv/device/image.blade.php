050b4ef903eaa574fb99fc988e4162dd6a44819d
datnk-hust <datnk@telemed.vn>
2020-06-30 00:17:30 +0700

    sua code 29/06 lan 4

--- resources/views/ktv/device/image.blade.php
+++ resources/views/ktv/device/image.blade.php
@@ -11,7 +11,6 @@
 	<div>	
 		<img src="/asset/group{{ $devices->group }}/{{ $devices->dv_type_id }}/{{ $devices->handover_img }}" alt="{{ $devices->handover_img }}" />
 	</div><br>
-	<!-- <div><a href="http://localhost/myproject/public/asset/{{ $devices->handover_img }}"><button class="btn btn-primary">Click để mở ảnh</button></a></div> -->
 </body>
 </html>
 
